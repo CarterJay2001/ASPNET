6542fa1a4ec7dfe14f35259de842dc5e7922f913
Jaylon Carter <carterjay2001@outlook.com>
2022-02-14 14:19:12 -0600

    added products, working on categories

---
+++ Testing/Models/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Testing.Models;
+namespace Testing
+{
+    public interface IProductRepository
+    {
+        public IEnumerable<Product> GetAllProducts();
+        public Product GetProduct(int id);
+        public void UpdateProduct(Product product);
+        public IEnumerable<Category> Categories { get; set; }
+        public IEnumerable<Category> GetCategories();
+        public Product AssignCategory();
+    }
+}
